bf6b5bb84c1735bdb12bde6b62285d60be354f1d
Felix developer <felixxdeveloper@gmail.com>
2022-08-11 19:50:20 -0300

    REF - Better the security

--- src/useCases/create/channel/subscribe/createSubscribe.ts
+++ src/useCases/create/channel/subscribe/createSubscribe.ts
@@ -1,11 +1,23 @@
-import { Enrolled } from '../../../../models';
 import { EnrolledProps } from '../../../../interfaces';
+import { Enrolled, Channels } from '../../../../models';
 
 export async function createSubscribe(subscribe: EnrolledProps): Promise<Error | EnrolledProps> {
   try {
+    const channel = await Channels.findOne({ user_id: subscribe.user_id });
+
+    if (!channel) {
+      const result = await Enrolled.create<EnrolledProps>(subscribe);
+
+      return result;
+    }
+
+    if (channel._id === subscribe.channel_id)
+      return new Error('Error, Cannot subscribe to the channel itself');
+
     const result = await Enrolled.create<EnrolledProps>(subscribe);
 
     return result;
+
   } catch (error) {
     return new Error('Error subscribing');
   }
