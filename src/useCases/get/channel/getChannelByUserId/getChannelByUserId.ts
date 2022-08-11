b7b1ddb0a47cfd807b7e8d73ea2cf272be7a8522
Felix developer <felixxdeveloper@gmail.com>
2022-08-11 19:49:38 -0300

    ADD - adding subscribes users endpoint

--- src/useCases/get/channel/getChannelByUserId/getChannelByUserId.ts
+++ src/useCases/get/channel/getChannelByUserId/getChannelByUserId.ts
@@ -2,19 +2,19 @@ import { Channels } from "../../../../models";
 import { Channel } from "../../../../interfaces";
 import { client } from '../../../../db-redis';
 
-export async function getChannelById(company_id: string): Promise<Error | Channel | null> {
+export async function getChannelById(channel_id: string): Promise<Error | Channel | null> {
   try {
-    const result = await client.get(company_id);
+    const result = await client.get(channel_id);
 
     if (result) {
       const channel: Channel = JSON.parse(result);
       return channel;
     }
 
-    const channel = await Channels.findOne<Channel>({ id: company_id });
+    const channel = await Channels.findOne<Channel>({ id: channel_id });
 
     if (channel)
-      await client.set(company_id, JSON.stringify(channel));
+      await client.set(channel_id, JSON.stringify(channel));
 
     return channel;
 
--- src/useCases/get/channel/getChannelByUserId/getChannelByUserIdController.ts
+++ src/useCases/get/channel/getChannelByUserId/getChannelByUserIdController.ts
@@ -4,12 +4,14 @@ import { getChannelById } from './getChannelByUserId';
 
 export async function getChannelByUserIdController(request: Request, response: Response) {
   try {
-    const company_id = await decodeId(request.headers.authorization);
+    const IDs = await decodeId(request.headers.authorization);
 
-    if (company_id instanceof Error)
+    if (IDs instanceof Error)
       return response.sendStatus(401);
 
-    const result = await getChannelById(company_id);
+    const { channel_id } = IDs;
+
+    const result = await getChannelById(channel_id);
 
     if (result instanceof Error)
       return response.status(400).json(result.message);
