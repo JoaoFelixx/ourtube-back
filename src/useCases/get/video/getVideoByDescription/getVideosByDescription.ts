c4e645730dd130880966ffa59470850e5b7828f4
Felix developer <felixxdeveloper@gmail.com>
2022-08-12 20:07:45 -0300

    REF - Added populate function for more data

--- src/useCases/get/video/getVideoByDescription/getVideosByDescription.ts
+++ src/useCases/get/video/getVideoByDescription/getVideosByDescription.ts
@@ -4,7 +4,8 @@ import { Videos } from "../../../../models";
 export async function getVideoByDescription(description: string): Promise<Error | Video[]> {
   try {
     return await Videos.find<Video>({ description: { $regex: description, $options: 'i' } })
-      .limit(5);
+      .limit(5)
+      .populate('channel_id');
 
   } catch (error) {
     return new Error('Error getting video by description')
