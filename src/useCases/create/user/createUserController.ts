0fc5a97198945b00c9cd66d992379894b88c6aa3
Felix developer <felixxdeveloper@gmail.com>
2022-09-02 15:26:58 -0300

    REF - Verifying if email is valid

--- src/useCases/create/user/createUserController.ts
+++ src/useCases/create/user/createUserController.ts
@@ -1,17 +1,22 @@
 import bcrypt from 'bcryptjs';
-import { Request, Response } from 'express';
-import { createUser } from './createUser';
 import { User } from '../../../interfaces';
+import { createUser } from './createUser';
+import { Request, Response } from 'express';
 import { randomUUID as uuid } from 'crypto';
 
 export async function createUserController(request: Request, response: Response) {
   try {
+    const regexEmail = new RegExp(/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/);
+
     const user: User = {
       _id: uuid(),
       email: request.body.email.toLowerCase(),
-      password: await bcrypt.hash(request.body.password, 8)
+      password: await bcrypt.hash(request.body.password, 8),
     }
 
+    if (!regexEmail.test(user.email))
+      return response.status(400).json('Email is invalid');
+    
     const result = await createUser(user);
 
     if (result instanceof Error)
