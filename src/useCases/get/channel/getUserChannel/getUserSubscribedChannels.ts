371f98606796b98e4b083db7fcd2e9e9237184f1
Felix developer <felixxdeveloper@gmail.com>
2022-09-05 14:16:50 -0300

    REF - populate with channel__id

--- src/useCases/get/channel/getUserChannel/getUserSubscribedChannels.ts
+++ src/useCases/get/channel/getUserChannel/getUserSubscribedChannels.ts
@@ -1,8 +1,9 @@
-import { EnrolledProps } from '../../../../interfaces';
 import { Enrolled, } from '../../../../models';
+import { EnrolledProps } from '../../../../interfaces';
 
 export async function getUserSubscribedChannels(user_id: string): Promise<EnrolledProps[]> {
-  const result = await Enrolled.find<EnrolledProps>({ user_id });
+  const result = await Enrolled.find<EnrolledProps>({ user_id })
+    .populate('channel_id');
 
   return result;
 }
