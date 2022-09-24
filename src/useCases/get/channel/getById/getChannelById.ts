dd320b83434b90835750f2a95f1694262f412216
Felix developer <felixxdeveloper@gmail.com>
2022-09-24 12:26:03 -0300

    REF - removing comments

--- src/useCases/get/channel/getById/getChannelById.ts
+++ src/useCases/get/channel/getById/getChannelById.ts
@@ -1,6 +1,5 @@
 import { Enrolled, Channels } from "../../../../models";
 import { Channel, EnrolledProps } from "../../../../interfaces";
-//import { client } from '../../../../db-redis';
 
 interface ChannelAndEnrolled extends Channel {
   enrolled: EnrolledProps[];
@@ -12,14 +11,6 @@ interface ChannelOptions {
 
 export async function getChannelById(_id: string): Promise<ChannelAndEnrolled | Error | null> {
   try {
-    /* const result = await client.get(_id);
-
-    if (result) {
-      const channel: Channel = JSON.parse(result);
-      
-      return channel;
-    }*/
-
     const channel = await Channels.findOne<Channel & ChannelOptions>({ _id });
 
     if (!channel)
@@ -31,9 +22,6 @@ export async function getChannelById(_id: string): Promise<ChannelAndEnrolled | 
       ...channel.toObject(),
       enrolled: [...enrolled]
     }
-
-    /* if (channel)
-      await client.set(_id, JSON.stringify(channel)); */
 
     return channelWithEnrolled;
 
